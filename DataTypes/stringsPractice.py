f9a98318ae6cf4d57d9ab1eb8a8406b3d9eb733e
Neehar Gadam <neehargadam@gmail.com>
2024-12-20 19:43:22 +0530

    string practice

---
+++ DataTypes/stringsPractice.py
@@ -0,0 +1,55 @@
+s1='n'
+print(s1)
+print(type(s1))
+
+s2="g"
+print(s2)
+print(type(s2))
+
+s3="Neehar"
+print(s3)
+print(type(s3))
+
+s4='Gadam'
+print(s4)
+print(type(s4))
+
+s5="7Neehar9999"
+print(s5)
+print(type(s5))
+
+
+
+
+
+
+
+
+
+addr1="""\t Ayyappa Colony,
+         Shadnagar,
+         Rangareddy District,
+         Telangana State,
+         509216"""
+print(addr1)
+print(type(addr1))
+
+addr2='''\t RTC Colony,
+         Shadnagar,
+         Rangareddy District,
+         Telangana State,
+         509216'''
+print(addr2)
+print(type(addr2))
+
+
+python="""Python is a high-level, interpreted, \ninteractive and object-oriented scripting language. \nPython is designed to be highly readable. \nIt uses English keywords frequently where as other languages use punctuation, \nand it has fewer syntactical constructions than other languages."""
+print(python)
+
+s="Python Programming"
+print(len(s))
+print(s[0])
+print(s[len(s)-1])
+
+
+ 
