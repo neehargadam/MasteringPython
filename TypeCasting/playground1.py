841b49bb05a33d5694fda9974267dbdde1091baa
Neehar Gadam <neehargadam@gmail.com>
2024-12-24 19:56:12 +0530

    type casting practice

---
+++ TypeCasting/playground1.py
@@ -0,0 +1,12 @@
+a=True
+print(a)
+print(type(a))
+
+b=int(a)
+print(b)
+print(type(b))
+
+c=2+3j
+print(c)
+d=int(c)
+print(d)
---
+++ TypeCasting/theory.py
@@ -0,0 +1,11 @@
+# int()
+# float()
+# bool()
+# complex()
+# str()
+
+# "123" --> 
+# "12.34"
+# "True"
+# "1+2j"
+# "Python"
